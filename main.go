208a2a778803adf2020f48ac49e457f671d97aa5
Aleksandr Panfilov <aaa2ppp@gmail.com>
2025-07-11 17:16:44 +0300

    Add configurable timeouts

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"cmp"
+	"context"
 	"crypto/tls"
 	"log"
 	"log/slog"
@@ -11,6 +12,7 @@ import (
 	"os"
 	"path"
 	"strings"
+	"time"
 
 	"green-api-proxy/www"
 
@@ -20,9 +22,16 @@ import (
 func main() {
 	godotenv.Load()
 
-	serverAddr := cmp.Or(os.Getenv("SERVER_ADDR"), ":8087")
-	allowOrigin := os.Getenv("ALLOW_ORIGIN")
-	greenAPIBaseURL := cmp.Or(os.Getenv("GREEN_API_BASE_URL"), "https://api.green-api.com")
+	setupLogger()
+
+	var (
+		serverAddr      = cmp.Or(os.Getenv("SERVER_ADDR"), ":8087")
+		allowOrigin     = os.Getenv("ALLOW_ORIGIN")
+		greenAPIBaseURL = cmp.Or(os.Getenv("GREEN_API_BASE_URL"), "https://api.green-api.com")
+		readTimeout     = getTimeout("READ_TIMEOUT", 1*time.Second)
+		writeTimeout    = getTimeout("WRITE_TIMEOUT", 3*time.Second)
+		redirectTimeout = getTimeout("REDIRECT_TIMEOUT", 2*time.Second)
+	)
 
 	// Настраиваем прокси
 	proxy := &httputil.ReverseProxy{
@@ -44,17 +53,22 @@ func main() {
 	}
 
 	// Настраиваем сервер
-	http.Handle("/green-api/", http.StripPrefix("/green-api", http.FileServerFS(www.Static)))
 
-	http.Handle("/green-api-proxy/", http.StripPrefix("/green-api-proxy",
+	mux := http.NewServeMux()
+	mux.Handle("/green-api/", http.StripPrefix("/green-api", http.FileServerFS(www.Static)))
+
+	mux.Handle("/green-api-proxy/", http.StripPrefix("/green-api-proxy",
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx, cancel := context.WithTimeout(r.Context(), redirectTimeout*time.Millisecond)
+			defer cancel()
+
 			// Восстанавливаем ApiToken в пути запроса из заголовка
 			apiToken := r.Header.Get("X-ApiToken")
 			if apiToken == "" {
 				http.Error(w, "The 'X-ApiToken' header is required", http.StatusBadRequest)
 				return
 			}
-			r = r.Clone(r.Context()) // чтобы не изменять исходный запрос
+			r = r.Clone(ctx) // чтобы не изменять исходный запрос
 			r.Header.Del("X-ApiToken")
 			r.URL.Path = path.Join(r.URL.Path, apiToken)
 
@@ -77,9 +91,16 @@ func main() {
 		}),
 	))
 
+	server := &http.Server{
+		Addr:         serverAddr,
+		Handler:      mux,
+		ReadTimeout:  readTimeout,
+		WriteTimeout: writeTimeout,
+	}
+
 	// Запускаем сервер
-	log.Println("Сервер запущен на :8087 (HTTPS)")
-	log.Fatal(http.ListenAndServeTLS(serverAddr, "./cert/cert.pem", "./cert/key.pem", nil))
+	log.Printf("Сервер запущен на %s (HTTPS)", serverAddr)
+	log.Fatal(server.ListenAndServeTLS("./cert/cert.pem", "./cert/key.pem"))
 }
 
 func maskApiToken(url string) string {
@@ -102,4 +123,17 @@ func setupLogger() {
 	logger := slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: level}))
 	logger.Info("setup logger", "level", level)
 	slog.SetDefault(logger)
+}
+
+func getTimeout(env string, defaultValue time.Duration) time.Duration {
+	s, ok := os.LookupEnv(env)
+	if !ok {
+		return defaultValue
+	}
+	v, err := time.ParseDuration(s)
+	if err != nil {
+		log.Printf("ignore %s: %v", env, err)
+		return defaultValue
+	}
+	return v
 }
