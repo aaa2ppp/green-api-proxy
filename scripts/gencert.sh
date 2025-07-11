ac136154af4f0ecf4cd6d2d35c78dec5c431ec78
Aleksandr Panfilov <aaa2ppp@gmail.com>
2025-07-11 17:22:43 +0300

    docker: Expose APP_PORT and fix cert generation

--- scripts/gencert.sh
+++ scripts/gencert.sh
@@ -11,9 +11,11 @@ openssl genrsa -out ./cert/key.pem 4096
 # Генерируем самоподписанный сертификат (на 365 дней)
 case $(uname) in
 MINGW64_NT*)
-    openssl req -new -x509 -key ./cert/key.pem -out ./cert/cert.pem -days 365 -subj "//CN=${CN:-localhost}"
+    openssl req -new -x509 -key ./cert/key.pem -out ./cert/cert.pem -days 365 -subj "//CN=${CN:-localhost}" \
+        -addext "subjectAltName=DNS:${CN:-localhost},IP:${IP:-0.0.0.0}"
     ;;
 *)
-    openssl req -new -x509 -key ./cert/key.pem -out ./cert/cert.pem -days 365 -subj "/CN=${CN:-localhost}"
+    openssl req -new -x509 -key ./cert/key.pem -out ./cert/cert.pem -days 365 -subj "/CN=${CN:-localhost}" \
+        -addext "subjectAltName=DNS:${CN:-localhost},IP:${IP:-0.0.0.0}"
     ;;
 esac
